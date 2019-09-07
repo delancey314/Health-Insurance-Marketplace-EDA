a5e19f7002d74a1b94647a77a68d78881ca23559
Bobcat90 <delancey314@netzero.net>
2019-09-06 22:01:40 -0600

    22:00 06SEP. still doing initial attribute pull

--- pipeline.py
+++ pipeline.py
@@ -3,15 +3,18 @@ import numpy as np
 year=[2014,2015,2016]
 files=[]
 
-def make_attributes(year=2014,file='Benefits_Cost_Sharing_PUF.csv'):
-    read_file=pd.read_csv(f'data/{year}/{file}',low_memory=False)
-    new_fields=list(read_file.columns)
-    uniques=[]
-    for field in new_fields:
-        col=new_fields[field]
-        uniques.append(col.unique())
+#def make_attributes(year=2014,file='Benefits_Cost_Sharing_PUF.csv'):
+year=2014
+file='Benefits_Cost_Sharing_PUF.csv'
+cost=pd.read_csv(f'data/{year}/{file}',low_memory=False)
+headers=list(cost.columns)
+uniques=[]
+for header in headers:
+    col=cost[header]
+    uniques.append(col.unique())
 
-    uniquedict=dict(zip(headers,uniques))
+uniquedict=dict(zip(headers,uniques))
+    
 '''
 for header in headers:
     attr=uniquedict[header]   
