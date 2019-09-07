e9843e1524e203b7f7de5901a31a49af187f2c1f
Bobcat90 <delancey314@netzero.net>
2019-09-06 23:18:02 -0600

    23:11 06SEP redone loader again

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,19 @@
 import pandas as pd 
 import numpy as np
 year=[2014,2015,2016]
-files=[]
+files = dict{1:'Benefits_Cost_Sharing_PUF.csv'\
+            ,2:'Business_Rules_PUF.csv'\
+            ,3:'Network_PUF.csv'\
+            ,4:'Plan_Attributes_PUF_2014_2015-03-09.csv'\
+            ,5:'Rate_PUF.csv'
+            ,6: 'Service_Area_PUF.csv'\
+            }
+
+
+
+#fileload_counter determines whether to send a welcome message when file_chooser() is called.
+fileloader_counter = 0
+
 
 #def make_attributes(year=2014,file='Benefits_Cost_Sharing_PUF.csv'):
 year=2014
@@ -62,4 +74,25 @@ for header in headers:
 
 attr=uniquedict['SourceName']   
 np.sort(attr) 
+'''
+def file_chooser(num_files= 1, year=2014, file ='data/Benefits_Cost_Sharing_PUF.csv'):
+     This function allows the user to select the years and files to use
 
+    vars:
+
+    fileloader_counter = 0 displays a welcome. Otherwise it goes to the selection portion.
+    year = the user entered year
+    file_list = the available files
+    file = the user chosen file
+
+
+    
+    if fileloader_counter == 0:
+        print('Welcome to the CMS Marketplace dataset. The first step is to load\n \
+            files. There are 3 default years and 6 default files')
+    fileloader_counter +=1
+    while year != 'q':
+        year = input("The default years are 2014, 2015, and 2016. Please enter \n\
+            the year you wish to use as an integer or 'q' to quit->")
+
+'''
