f1acdbf8dfd8cdfb4fda554cfe9799560b1862de
Bobcat90 <delancey314@netzero.net>
2019-09-11 16:39:42 -0600

    cohabitation broken out and  EDA datasets started

--- file_cleaner.py
+++ file_cleaner.py
@@ -1,4 +1,27 @@
 import pandas as pd 
+import csv
+import numpy as np
+
+def cohab_unique():
+    with open('cohabitation.csv', 'r') as csvfile:
+        test_import = csv.reader(csvfile,delimiter=';')
+        import_dict={}
+        for line in test_import:
+            for value in line:
+                output_list =value.split(',')
+                import_dict[output_list[0]]=output_list[1]
+        return list(import_dict.keys())
+        
+def hot_cat_cohabitation(cohabitation_fields,cohab):
+    index=len(cohabitation_fields)
+    for idx in range(index):
+        new_yes = cohabitation_fields[idx] +',Yes'
+        new_no= cohabitation_fields[idx]+',No'
+        new_cat=cohabitation_fields[idx]+'-YN'
+        cohab[new_yes]= cohab['CohabitationRule'].apply(lambda r: int(str(r).find(new_yes)>=0))
+        cohab[new_no] = cohab['CohabitationRule'].apply(lambda r: int(str(r).find(new_no)>=0))
+        cohab[new_cat]=  np.where((cohab[new_yes] +  cohab[new_no]) >0, 1, 0)
+    return cohab
 
 def file_cleaner():
     clean_rules(rules_all)
@@ -13,52 +36,43 @@ def clean_rules(cleaning_file):
     #Data Dictionary says to replace any blanks/NaN with No
     cleaning_file['DentalOnlyPlan'].fillna('No', inplace=True)
     cleaning_file.reset_index()
-    cohabitation_matrix(cleaning_file)
+  
+    
+    
     return cleaning_file
 
    
 
-
-""" def cohabitation_matrix(cleaning_file):
-    cohab = cleaning_file
+def cohabitation_matrix(rules_cleaned):
+    cohab = rules_cleaned
     #cohab.drop('EnrolleeContractRateDeterminationRule','MinimumTobaccoFreeMonthsRule','DentalOnlyPlan')
-    cohab_categories_yes=['Father or Mother']
-    
-    for category in cohab_categories_yes:
-        cohab[category]= cohab['CohabitationRule'].apply(lambda r: int(str(r).find(category)>=0))
-    return cohab
+    cohabitation_fields=cohab_unique()
+    cohab_new_matrix=hot_cat_cohabitation(cohabitation_fields,cohab)
 
-    import_list=[]
-    import_dict={}
-    with open('cohabitation.csv', 'r') as csvfile:
-        test_import = csv.reader(csvfile,delimiter=';')
-        for line in test_import:
-            for value in line:
-                output_list =value.split(',')
-                import_dict[output_list[0]]=output_list[1]
-    Cohabitation_hot=import_dict.keys()
-    cohab_list=[]
-    index=len(Cohabitation_hot)
-f   or idx in index:
+    return cohab_new_matrix
 
-        new_yes = Cohabitation_hot[idx] +', Yes'
-        new_no= Cohabitation_hot[idx]+', No'
-        new_cat=Cohabitation_hot[idx]+'-YN'
-        cohab[new_yes]= cohab['CohabitationRule'].apply(lambda r: int(str(r).find('new_yes')>=0))
-        cohab[new_no] = cohab['CohabitationRule'].apply(lambda r: int(str(r).find('new_no')>=0))
-        cohab[new_cat]=  np.where(cohab[new_yes] ==1 or cohab[new_no]==1, 1, 0)
-    
-""" 
-rules_all=pd.read_csv('data/merged/rules_all.csv',low_memory=False)
-test_arr = rules_all['CohabitationRule'].unique()
-test_lst = test_arr.tolist()
-#rules_cleaned=clean_rules(rules_all)
 
+
+'''
 cohab_list=[]
 cohab_dict={}
 for item in test_lst:
         output_list =str(item).split(',')
         cohab_dict[output_list[0]]=output_list[1]
-Cohabitation_hot=cohab_dict.keys()
+cohabitation_fields=cohab_dict.keys()
 
-index=len(Cohabitation_hot)
+index=len(cohabitation_fields)
+'''
+    
+
+rules_all=pd.read_csv('data/merged/rules_all.csv',low_memory=False)
+
+rules_cleaned=clean_rules(rules_all)
+cohabitation_df=cohabitation_matrix(rules_cleaned)
+cohabitation_df['DomParAsSpouse']= cohabitation_df['DomesticPartnerAsSpouseIndicator'].apply(lambda x: 1 if x=='Yes'  else 0)
+
+
+
+
+
+
